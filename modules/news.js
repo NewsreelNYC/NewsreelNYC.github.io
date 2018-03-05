f6729af24388c91fd29552ea5ae3319bac07c527
Pratham Gandhi <pratham_gandhi@horacemann.org>
2018-03-05 16:33:03 -0500

    implemented search everywhere:

--- modules/news.js
+++ modules/news.js
@@ -118,6 +118,6 @@ function search(){
   var searchbar = document.getElementById('searchbar');
   var query = searchbar.value;
   console.log(query);
-  window.location.href = "../Search/Search.html?" + query;
+  window.location.href = "/pages/Search/Search.html?" + query;
 }
 
