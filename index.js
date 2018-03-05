e51f5011ba42c6769ac4da0b4bac1331e3492060
Pratham Gandhi <pratham_gandhi@horacemann.org>
2018-03-05 16:35:22 -0500

    forgot search on index

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("loaded");
+function search(){
+  var searchbar = document.getElementById('searchbar');
+  var query = searchbar.value;
+  console.log(query);
+  window.location.href = "/pages/Search/Search.html?" + query;
+}
