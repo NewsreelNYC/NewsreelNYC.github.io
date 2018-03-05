8e2baefccc2029316781892054ceca171b804419
Pratham Gandhi <pratham_gandhi@horacemann.org>
2018-03-05 16:36:28 -0500

    route to search page was broken

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ function search(){
   var searchbar = document.getElementById('searchbar');
   var query = searchbar.value;
   console.log(query);
-  window.location.href = "/pages/Search/Search.html?" + query;
+  window.location.href = "/pages/News/Search/Search.html?" + query;
 }
