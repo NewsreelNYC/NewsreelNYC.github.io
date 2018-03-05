748ec388ecc30530443065621145aeaf9f5b8b60
Pratham Gandhi <pratham_gandhi@horacemann.org>
2018-03-05 16:57:46 -0500

    added search on search results page

--- pages/News/Search/Search.js
+++ pages/News/Search/Search.js
@@ -143,5 +143,10 @@ function htmlToElement(html) {
 
   });
 
-
+function search(){
+  var searchbar = document.getElementById('searchbar');
+  var query = searchbar.value;
+  console.log(query);
+  window.location.href = "/pages/News/Search/Search.html?" + query;
+}
 
